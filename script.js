dc4401304cd43592f374570b8edf552be86f2dd1
johann gomez <johanngomez02@gmail.com>
2021-05-01 10:02:42 -0500

    Comentarios, senticias, variables, constantes y uso de let

---
+++ script.js
@@ -0,0 +1,65 @@
+// Hola mundo
+//alert('hola mundo!');
+
+// Comentarios
+ //comentarios de linea
+ /*comentarios de mas 
+ de una lineas*/ 
+
+// Crea una página que muestre el mensaje en el html 'Soy JavasScript'
+//document.write('Soy JavaScript');
+
+// Sentencias
+//alert('Hola'); alert('mundo');
+    //alert('Hola'); 
+    //alert('mundo');
+        // Inserción automatica de punto y coma
+        //alert('Hola') 
+        //alert('mundo')
+/*-----------------------------------------------------------------------------*/
+// Variables
+
+let mensaje;
+mensaje = "hola mundo"
+alert(mensaje)
+
+let a = 5;
+let b = 6;
+let suma = a + b;
+console.log("El resultado de la suma es: ",suma);
+
+let nombre = "Sowil";
+let nombre2 = nombre;
+
+nombre2 = "Muñoz";
+console.log("El nombre uno es: ",nombre);
+console.log("El nombre dos es: ",nombre2);
+
+// Nombrar variables con camel case
+nombreUno = "Sowil";
+nombreDos = "Muñoz";
+
+// Nombrar como underscore
+nombre_uno = "Sowil";
+nombre_Dos = "Muñoz";
+
+// nombres no comunes 
+let $ = 1;
+let _ = 2;
+
+/* no puedes nombrar variables de esta manera
+let 1a = 3;
+let primer-nombre = 4; */
+
+'use stric'
+mensaje = 'hola mundo';
+
+// Constantes
+const PI = 3.141624;
+const COLOR_ROJO = "#FFFFF";
+pi_dos = pi;
+pi_dos = 7;
+console.log(pi);
+console.log(pi_dos);
+
+const id_account = get_id_account(45);
