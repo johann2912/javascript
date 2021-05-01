976abb6af51fb4e1a90feca6983281dabd2074f4
johann gomez <johanngomez02@gmail.com>
2021-05-01 11:33:52 -0500

    Conversión entre tipos de datos

--- script.js
+++ script.js
@@ -26,14 +26,14 @@ alert(mensaje)
 let a = 5;
 let b = 6;
 let suma = a + b;
-console.log("El resultado de la suma es: ",suma);
+//console.log("El resultado de la suma es: ",suma);
 
 let nombre = "Sowil";
 let nombre2 = nombre;
 
 nombre2 = "Muñoz";
-console.log("El nombre uno es: ",nombre);
-console.log("El nombre dos es: ",nombre2);
+//console.log("El nombre uno es: ",nombre);
+//console.log("El nombre dos es: ",nombre2);
 
 // Nombrar variables con camel case
 nombreUno = "Sowil";
@@ -59,8 +59,8 @@ const PI = 3.141624;
 const COLOR_ROJO = "#FFFFF";
 pi_dos = PI;
 pi_dos = 7;
-console.log(PI);
-console.log(pi_dos);
+//console.log(PI);
+//console.log(pi_dos);
 
 // const id_account = get_id_account(45);
 
@@ -77,19 +77,19 @@ n = 12.456;
 // Valores numericos especiales Infiniti, -Infinite y NaN
 
 // Infinity: representa el infinito matematico. Es un valor especial que es mayor que cualquier número;
-console.log(1 / 0);
-console.log(Infinity);
+//console.log(1 / 0);
+//console.log(Infinity);
 
 // NaN: Representa un error de cálculo. Es el resultado de una operación matemtica incorrecta o indefinida
-console.log("No es un número" / 2);
+//console.log("No es un número" / 2);
 
 // Nan es conocido como pegajoso. Cualquir operación sobre NaN se vuelve NaN
-console.log("No es un número" / 2 + 5);
+//console.log("No es un número" / 2 + 5);
 
 // Tipo de datos BigInt
 // Valores enteros mayores que (2^53-1) o menor que -(2^53-1) 9.007.199.254.740.991
 let numero_grande = 12345678901234567891299955456366458312n;
-console.log(numero_grande);
+//console.log(numero_grande);
 
 // Tipo String
 let t = "a";
@@ -101,9 +101,9 @@ let nomb = "Sowil";
 //console.log('hola', nomb);
 //console.log(`Hola`, nomb);
 
-console.log("hola ${nomb}");
-console.log('hola ${nomb}');
-console.log(`hola ${nomb}`);
+//console.log("hola ${nomb}");
+//console.log('hola ${nomb}');
+//console.log(`hola ${nomb}`);
 
 // Tipo Booleano
 
@@ -117,7 +117,7 @@ let mayor = 4 > 1; // True
 // Es un valor especial que no pertenece a ningun tipo de dato anterior.
 // Forma un tipo propio separado que contiene sólo el vvalor null.
 let edad = null;
-console.log(edad);
+//console.log(edad);
 
 // En JS NULL NO SIGNIFICA UN OBJETO NO EXISTENTE 
 // Es solo un valor especial que representa nada, vacío o valor desconocido.
@@ -127,15 +127,81 @@ console.log(edad);
 // El siginificado es Undefined es valor no asignado.
 
 let edad_dos;
-console.log(edad_dos);
+//console.log(edad_dos);
 
 // El operador typeof o función typeof(x), devuelve el tipo de dato 
 
-console.log(typeof undefined); //  Undefined
-console.log(typeof 0); // number
-console.log(typeof true); // boolean
-console.log(typeof 10n); //BigInt
-console.log(typeof "hola"); // string
+//console.log(typeof undefined); //  Undefined
+//console.log(typeof 0); // number
+//console.log(typeof true); // boolean
+//console.log(typeof 10n); //BigInt
+//console.log(typeof "hola"); // string
 
-console.log(typeof(alert));
-console.log(typeof(null));
+//console.log(typeof(alert)); // function
+//console.log(typeof(null)); // object -  ERROR DE JAVASCRIPT, PORQUE ESTO DEBERIA ENVIAR NULL
+//console.log(typeof(Math)); // object
+
+// Interacciones: alert, prompt, confirm
+
+// alert - tambien es conocido como ventana modal
+//alert('hola mundo');
+
+// prompt
+//let nonbre_capturado = prompt('Digite el nombre', 'Hola');
+//alert(nombre_capturado);
+
+//prompt('Digite el nombre')
+//prompt('Digite el nombre', '') // Solo para Internet Explore
+
+// Confirm
+// let esHelado = confirm('¿Tegusta el helado');
+// console.log(esHelado); // true si presionan OK y false si presionan cancelar
+
+// CONVERSIÓN DE TIPOS
+// Existen dos tipos de conversiones explicita e implicita
+
+// Convertir a String 
+let valor = true;
+console.log(typeof valor);
+
+valor = String(valor); // "true"
+valor = String(false); // "false"
+valor = String(null); // "null"
+valor = String(undefined); // undefined
+console.log(typeof valor);
+
+//Convertir a numero
+
+// Conversiones númericas ocurren automáticamente en funciones matemáticas y expresiones
+// alert('6' / '2' ); // 3
+// alert(1 + '2'); // '12'
+// alert('2' + 1 ); // '12'
+
+let cadena = "123";
+let num_cad = Number(cadena); // 123
+
+console.log(Number('hola')); // NaN
+
+/* 
+undefined -> NaN
+null -> 0
+true -> 1
+false -> 0
+string '' -> 0
+string 'hola' -> NaN 
+*/
+
+//Convertir a booleano
+console.log(Boolean("hola"));
+/*
+undefined -> false
+null -> false
+string '' -> false
+NaN -> false
+0 -> false
+
+'0' -> true
+'  ' -> true
+*/ 
+console.log(Boolean(String(0)))
+console.log(Boolean(0))
